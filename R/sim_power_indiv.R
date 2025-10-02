c3478f1d622c344db6c932ffc1a58157b1f8fe1d
Ryan Sun <ryanrsun@gmail.com>
2025-10-02 15:54:20 -0500

    cran version

--- R/sim_power_indiv.R
+++ R/sim_power_indiv.R
@@ -1,7 +1,7 @@
-#' sim_stats_indiv.R
+#' sim_power_indiv.R
 #'
-#' Simulate distribution of test statistics from individual data for
-#' multiple explanatory factor setting.
+#' Simulate power starting from individual-level data for multiple
+#' explanatory factor setting.
 #'
 #' @param B Number of simulations.
 #' @param sigSq Variance of outcome.
@@ -9,27 +9,33 @@
 #' @param gMat Matrix of genotypes, n*J.
 #' @param alphaVec p*1 vector of regression coefficients for xMat.
 #' @param betaVec J*1 vector of regression coefficients for gMat.
-#' @param checkpoint Report every 50 simulations.
+#' @param decompTrue The return value of a call to eigen() on the true covariance matrix. Can be null,
+#' in which case estimated covariance will be used.
+#' @param checkpoint Boolean, if true then print message every 50 simulations.
 #'
 #' @return A list with the elements:
-#' \item{tMat}{B*J matrix of T statistics.}
-#' \item{zMat}{B*J matrix of Z statistics.}
-#' \item{zVecGBJ}{Vector of Z statistics should match first row of zMat.}
-#' \item{iMat}{Innovated statistics using correlation matrix under the null.}
+#' \item{zMat}{B*J matrix of test statistics Z.}
+#' \item{zVecGBJ}{Check on Z statistics, vector should match first row of zMat.}
+#' \item{iMat}{Innovated statistics matrix also of dimension B*J.}
+#' @importFrom stats glm
+#' @importFrom stats gaussian
+#' @importFrom stats rnorm
 #' @export
 #' @examples
+#' \donttest{
 #' xMat <- cbind(1, rnorm(n = 1000), rbinom(n = 1000, size=1, prob=0.5))
 #' gMat <- matrix(data = rbinom(n=10000, size=2, prob=0.3), nrow=1000)
 #' alphaVec <- c(1, 1, 1)
 #' betaVec <- rep(0, 10)
 #' sim_stats_mef(B=10000, sigSq = 1, xMat = xMat, gMat = gMat, alphaVec = alphaVec, betaVec = betaVec)
-sim_stats_mef <- function(B, sigSq, xMat, gMat, alphaVec, betaVec, checkpoint = FALSE) {
+#' }
+sim_stats_mef <- function(B, sigSq, xMat, gMat, alphaVec, betaVec, decompTrue = NULL, checkpoint = FALSE) {
 
-  # mean vector
+  # true mean vector
   etaVec <- xMat %*% alphaVec + gMat %*% betaVec
   n <- length(etaVec)
   p <- ncol(xMat)
-  q <- ncol(gMat)
+  J <- ncol(gMat)
   # quantities for calculating statistics and correlation matrix
   pMat <- xMat %*% solve(t(xMat) %*% xMat) %*% t(xMat)
   mafVec <- apply(gMat, 2, mean) / 2
@@ -38,8 +44,8 @@ sim_stats_mef <- function(B, sigSq, xMat, gMat, alphaVec, betaVec, checkpoint = 
   GGterm <- t(gMat) %*% IminusP %*% gMat
 
   # record
-  tMat <- matrix(data=NA, nrow=B, ncol=q)
-  zMat <- matrix(data=NA, nrow=B, ncol=q)
+  tMat <- matrix(data=NA, nrow=B, ncol=J)
+  zMat <- matrix(data=NA, nrow=B, ncol=J)
   for (sim_it in 1:B) {
     # generate outcome
     yVec <- etaVec + rnorm(n=n, mean = 0, sd = sqrt(sigSq))
@@ -53,12 +59,11 @@ sim_stats_mef <- function(B, sigSq, xMat, gMat, alphaVec, betaVec, checkpoint = 
     if (sim_it == 1) {
       nullMod <- glm(yVec ~ xMat - 1, family=gaussian())
       #packageOutput <- calc_score_stats(null_model = nullMod, factor_matrix = gMat, link_function = "linear")
-      packageOutput <- calc_score_stats(null_model = nullMod, factor_matrix = gMat, link_function = "linear", P_mat = sigSqHat * IminusP)
+      packageOutput <- GBJ::calc_score_stats(null_model = nullMod, factor_matrix = gMat, link_function = "linear")
       zVecGBJ <- packageOutput$test_stats
     }
 
     # record
-    tMat[sim_it, ] <- tVec
     zMat[sim_it, ] <- zVec
 
     # checkpoint
@@ -71,43 +76,55 @@ sim_stats_mef <- function(B, sigSq, xMat, gMat, alphaVec, betaVec, checkpoint = 
   denomVec <- 1 / sqrt(diag(GGterm))
   corMat <- diag(denomVec) %*% GGterm %*% diag(denomVec)
   decomp <- eigen(corMat)
-  iMat <- zMat %*% decomp$vectors %*% diag(1 / sqrt(decomp$values))
+  iMatEst <- zMat %*% decomp$vectors %*% diag(1 / sqrt(decomp$values))
 
-  return(list(tMat = tMat, zMat = zMat, zVecGBJ = zVecGBJ, iMat = iMat))
+  # innovate with true correlation matrix
+  if (!is.null(decompTrue)) {
+    iMat <- zMat %*% decompTrue$vectors %*% diag(1 / sqrt(decompTrue$values))
+  } else {
+    iMat <- NULL
+  }
+
+  return(list(zMat = zMat, zVecGBJ = zVecGBJ, iMat = iMat, iMatEst = iMatEst))
 }
 
 
 
-#' Simulate power of detection boundary tests starting from individual data for
-#' multiple outcomes setting.
+#' Simulate power starting from individual-level data for multiple
+#' outcomes setting.
 #'
 #' @param B Number of simulations.
-#' @param sigSqVec Variance of outcomes.
+#' @param covY Covariance matrix of outcomes.
 #' @param xMat Design matrix of non-genetic covariates, n*p.
 #' @param gVec n*1 vector of genotypes.
 #' @param alphaMat p*K vector of regression coefficients for xMat.
 #' @param gammaVec K*1 vector of regression coefficients for each outcome.
-#' @param checkpoint Report every 50 simulations.
+#' @param checkpoint Boolean, if true then print message every 50 simulations.
 #'
 #' @return A list with the elements:
-#' \item{uMat}{Matrix of U statistics.}
-#' \item{vMat}{Matrix of V statistics.}
-#' \item{vVecGBJ}{Vector of V statistics should match first row of vMat}
+#' \item{zMat}{Matrix of test statistics Z.}
+#' \item{zVecGBJ}{Check on Z statistics, vector should match first row of zMat.}
 #' \item{iMat}{Innovated statistics using correlation matrix under the null.}
+#' @importFrom magrittr %>%
+#' @importFrom stats glm
+#' @importFrom stats gaussian
 #' @export
 #' @examples
+#' \donttest{
 #' covY <- matrix(data=0.3, nrow=10, ncol=10); diag(covY) <- 1
 #' xMat <- cbind(1, rnorm(n = 1000), rbinom(n = 1000, size=1, prob=0.5))
 #' gVec <- rbinom(n= 1000, size = 2, prob=0.3)
 #' alphaMat <-matrix(data = 1, nrow=3, ncol=10)
 #' gammaVec <- rep(0, 10)
-#' sim_stats_mo(B=10000, covY = covY, xMat = xMat, gVec = gVec, alphaMat = alphaMat, gammaVec = gammaVec)
+#' sim_stats_mo(B=10000, covY = covY, xMat = xMat, gVec = gVec,
+#' alphaMat = alphaMat, gammaVec = gammaVec)
+#' }
 sim_stats_mo <- function(B, covY, xMat, gVec, alphaMat, gammaVec, checkpoint = FALSE) {
 
-  # mean matrix n*K
+  # true mean matrix n*K
   etaMat <- xMat %*% alphaMat + gVec %*% t(gammaVec)
   n <- nrow(etaMat)
-  K <-length(gammaVec)
+  K <- length(gammaVec)
   p <- ncol(xMat)
 
   # quantities for calculating statistics and correlation matrix
@@ -117,7 +134,7 @@ sim_stats_mo <- function(B, covY, xMat, gVec, alphaMat, gammaVec, checkpoint = F
   varG <- t(gVec) %*% IminusP %*% gVec
   varYvec <- diag(covY)
   corY <- sweep(covY, MARGIN=2, STATS=sqrt(varYvec), FUN="/") %>%
-    sweep(., MARGIN=1, STATS=sqrt(varYvec), FUN="/")
+    sweep(MARGIN=1, STATS=sqrt(varYvec), FUN="/")
 
   # record
   uMat <- matrix(data=NA, nrow=B, ncol=K)
@@ -136,8 +153,8 @@ sim_stats_mo <- function(B, covY, xMat, gVec, alphaMat, gammaVec, checkpoint = F
       zVecPackage <- rep(0, K)
       for (k in 1:K) {
         nullMod <- glm(yMat[, k] ~ xMat - 1, family=gaussian())
-        #packageOutput <- calc_score_stats(null_model = nullMod, factor_matrix = gMat, link_function = "linear")
-        packageOutput <- calc_score_stats(null_model = nullMod, factor_matrix = matrix(data=rep(gVec, 2), ncol=2), link_function = "linear", P_mat = sigSqHat[k] * IminusP)
+        packageOutput <- GBJ::calc_score_stats(null_model = nullMod, factor_matrix = matrix(data=rep(gVec, 2), ncol=2), link_function = "linear")
+        #packageOutput <- calc_score_stats(null_model = nullMod, factor_matrix = matrix(data=rep(gVec, 2), ncol=2), link_function = "linear", P_mat = sigSqHat[k] * IminusP)
         zVecPackage[k] <- packageOutput$test_stats[1]
       }
     }
@@ -152,7 +169,7 @@ sim_stats_mo <- function(B, covY, xMat, gVec, alphaMat, gammaVec, checkpoint = F
     }
   }
 
-  # innovate with estimated correlation matrix under the null
+  # innovate with known correlation matrix
   decomp <- eigen(corY)
   iMat <- vMat %*% decomp$vectors %*% diag(1 / sqrt(decomp$values))
 
--- R/sim_power_mvn.R
+++ R/sim_power_mvn.R
@@ -2,16 +2,15 @@
 #'
 #' Simulate power of detection boundary tests starting from multivariate normal test statistics.
 #'
-#' @param B Number of simulations.
-#' @param muVec Mean vector of MVN (under the alternative).
-#' @param sigMat Covariance matrix of MVN (under the alternative).
-#' @param nullSigMat Assumed correlation matrix of MVN under the null.
-#' @param bounds A d*1 vector of bounds on the magnitudes of the test statistics, where
-#' the first element is the bound for |Z|_(1) and the last element is the bound for |Z|_(d).
-#' If provided, will calculate power from crossing the bounds.
+#' @param n Number of simulations.
+#' @param muVec Mean vector of test statistics under the alternative (assuming it's MVN).
+#' @param sigMat Covariance matrix of test statistics under the alternative (assuming it's MVN).
+#' @param bounds A J*1 vector of bounds on the magnitudes of the test statistics, where
+#' the first element is the bound for |Z|_(1) and the last element is the bound for |Z|_(J).
+#' @param nullSigMat Assumed correlation matrix of MVN under the null. Only need to specify if specifying test.
 #' @param test Either "GHC", "HC", "GBJ", or "BJ" or NULL. If provided, will calculate
 #' the p-value using the specified test and calculate power this way.
-#' @param alpha Level of the test
+#' @param alpha Level of the test.
 #'
 #' @return A list with the elements:
 #' \item{boundsPower}{Power from using bounds approach.}
@@ -19,27 +18,15 @@
 #'
 #' @export
 #' @examples
-#' myVariance <- matrix(data=0.3, nrow=5, ncol=5)
-#' diag(myVariance) <- 1
-#' myBounds <- set_GBJ_bounds(alpha = 0.01, d=5, sig_vec = myVariance[lower.tri(myVariance)])
-#' sim_power_mvn(B=1000, muVec = c(1, 0, 0, 0, 0), sigMat = myVariance, alpha=0.01)
+#' myCov <- matrix(data=0.3, nrow=5, ncol=5)
+#' diag(myCov) <- 1
+#' myBounds <- set_GBJ_bounds(alpha = 0.01, J=5, sig_vec = myCov[lower.tri(myCov)])
+#' sim_power_mvn(n=1000, muVec = c(1, 0, 0, 0, 0), sigMat = myCov, alpha=0.01)
 #'
-sim_power_mvn <- function(B, muVec, sigMat, nullSigMat=NULL, bounds=NULL, test=NULL, alpha) {
+sim_power_mvn <- function(n, muVec, sigMat, nullSigMat=NULL, bounds=NULL, test=NULL, alpha) {
 
   # simulate MVN
   zSample <- mvtnorm::rmvnorm(n=n, mean=muVec, sigma=sigMat)
-
-  # function to check if bounds are violated
-  # bounds should be smallest to largest
-  checkBoundsCross <- function(x, bounds) {
-    sortedX <- sort(abs(x))
-    numCross <- length(which(sortedX > bounds))
-    if (numCross == 0) {
-      return(0)
-    } else {
-      return(1)
-    }
-  }
 
   # if doing bounds
   if (!is.null(bounds)) {
@@ -52,13 +39,13 @@ sim_power_mvn <- function(B, muVec, sigMat, nullSigMat=NULL, bounds=NULL, test=N
   # if choosing a test, apply this function over zSample
   applyTest <- function(x, test, nullSigMat) {
     if (test == "GBJ") {
-      return(GBJ(test_stats = x, cor_mat = nullSigMat, pairwise_cors = NULL)$GBJ_pvalue)
+      return(GBJ::GBJ(test_stats = x, cor_mat = nullSigMat, pairwise_cors = NULL)$GBJ_pvalue)
     } else if (test == "BJ") {
-      return(BJ(test_stats = x, cor_mat = nullSigMat, pairwise_cors = NULL)$BJ_pvalue)
+      return(GBJ::BJ(test_stats = x, cor_mat = nullSigMat, pairwise_cors = NULL)$BJ_pvalue)
     } else if (test == "HC") {
-      return(HC(test_stats = x, cor_mat = nullSigMat, pairwise_cors = NULL)$HC_pvalue)
+      return(GBJ::HC(test_stats = x, cor_mat = nullSigMat, pairwise_cors = NULL)$HC_pvalue)
     } else if (test == "GHC") {
-      return(GHC(test_stats = x, cor_mat = nullSigMat, pairwise_cors = NULL)$GHC_pvalue)
+      return(GBJ::GHC(test_stats = x, cor_mat = nullSigMat, pairwise_cors = NULL)$GHC_pvalue)
     } else {
       return(NA)
     }
@@ -77,4 +64,16 @@ sim_power_mvn <- function(B, muVec, sigMat, nullSigMat=NULL, bounds=NULL, test=N
 }
 
 
-
+#' Internal function to check if bounds are violated.
+#' Bounds should be smallest to largest.
+#'
+#' @keywords internal
+checkBoundsCross <- function(x, bounds) {
+  sortedX <- sort(abs(x))
+  numCross <- length(which(sortedX > bounds))
+  if (numCross == 0) {
+    return(0)
+  } else {
+    return(1)
+  }
+}
